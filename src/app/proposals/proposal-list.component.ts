c5569f62199761cf4e664b07b333772b7db4c189
Ajith <ajithkgatty@gmail.com>
2018-05-27 14:39:55 +0530

    adding proposals class and creating list of proposals

--- src/app/proposals/proposal-list.component.ts
+++ src/app/proposals/proposal-list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal'
 
 @Component({
 	selector: 'proposals',
 	templateUrl: 'proposal-list.component.html'
 })
 
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(1, 'ABC Company', 'http://portfolio.abc.company.com', 'Ruby on Rails', 150, 100, 15, 'abc@gmail.com' )
+
+	proposalTwo: Proposal = new Proposal(1, 'GHJ Company', 'http://portfolio.fjs.company.com', 'Ruby on Rails', 100, 90, 15, 'ghfj@gmail.com' )
+
+	proposalThree: Proposal = new Proposal(1, 'ZXC Company', 'http://portfolio.Zxcv.company.com', 'Ruby on Rails', 100, 100, 25, 'abgjjc@gmail.com' )
+
+	proposals: Proposal[] = [this.proposalOne, this.proposalTwo, this.proposalThree ]
+}
 
 
 
