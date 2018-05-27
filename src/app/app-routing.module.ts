94c2aacad0e684371becc2c34380a47635579dbf
Ajith <ajithkgatty@gmail.com>
2018-05-27 18:38:19 +0530

    Added complete proposal form and some client validations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { DocumentsComponent } from './documents/documents.component'
 import { ProposalListComponent } from './proposals/proposal-list.component';
+import { ProposalNewComponent } from './proposals/proposal-new.component'
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/test', pathMatch: 'full' },
   { path: 'test', component: TestComponent },
   { path: 'documents', component: DocumentsComponent },
-  { path: 'proposals', component: ProposalListComponent }
+  { path: 'proposals', component: ProposalListComponent },
+  { path: 'proposals/new', component: ProposalNewComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposals/proposal-list.component';
+import { ProposalNewComponent } from './proposals/proposal-new.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     DocumentsComponent,
-    ProposalListComponent
+    ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
