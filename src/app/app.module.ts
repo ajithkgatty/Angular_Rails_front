964ac4a7b3c8a7f99ff2dd1b6caf7955574a2f2f
Ajith <ajithkgatty@gmail.com>
2018-05-29 17:07:17 +0530

    enabling bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposals/proposal-list.component';
-import { ProposalNewComponent } from './proposals/proposal-new.component'
-import { ProposalShowComponent } from './proposals/proposal-show.component'
+import { ProposalNewComponent } from './proposals/proposal-new.component';
+import { ProposalShowComponent } from './proposals/proposal-show.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,8 @@ import { ProposalShowComponent } from './proposals/proposal-show.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
